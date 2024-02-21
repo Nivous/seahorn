1248fc4457aa1c609fe628956c391867f4bd806b
Niv Sulimany <sulimany.niv@campus.technion.ac.il>
2024-02-21 12:31:27 +0200

    feat: getting max pc per function
    added a dictionary to get the max pc in a function, and also declared
    the getTraceRules function properly
    
    Signed-off-by: Niv Sulimany <sulimany.niv@campus.technion.ac.il>

--- include/seahorn/KPropertyVerifier.hh
+++ include/seahorn/KPropertyVerifier.hh
@@ -34,13 +34,15 @@ namespace seahorn
                                     std::map<const Function *, std::map<int, Expr>> &pc_rels,
                                     HornClauseDB::expr_set_type &pre_rules,
                                     ExprVector &bad_rules,
-                                    std::map<std::set<int>, Expr> &valid_rules);
+                                    std::map<std::set<int>, Expr> &valid_rules,
+                                    std::map<const Function *, int> &max_pc);
     void getHyperExprsModule(Module &M, HornifyModule &hm, ExprFactory &m_efac,
                           hyper_expr_map &k_vars, std::set<std::set<int>> &k_subsets,
                           std::map<const Function *, std::map<int, Expr>> &pc_rels,
                           HornClauseDB::expr_set_type &pre_rules,
                           ExprVector &bad_rules,
-                          std::map<std::set<int>, Expr> &valid_rules);
+                          std::map<std::set<int>, Expr> &valid_rules,
+                          std::map<const Function *, int> &max_pc);
     void getPcRels(const HornClauseDB::expr_set_type &orig_rels,
                     std::map<const Function *, std::map<int, Expr>> &new_rels,
                     ExprFactory &m_efac, Module &M, hyper_expr_map &k_rels,
@@ -58,11 +60,16 @@ namespace seahorn
     void getTraceInfo(Module &M, std::map<const Function *, std::map<std::pair<int, int>, ExprVector[3]>> &trace_info,
                       const HornClauseDB::expr_set_type &orig_rels, ExprFactory &m_efac,
                       const HornClauseDB::RuleVector &rules);
-
+    void getTraceRulesFromInfo(std::map<const Function *, std::map<std::pair<int, int>, ExprVector[3]>> &trace_info,
+                                hyper_expr_map &k_vars,
+                                std::map<const Function *, std::map<std::pair<int, int>, std::map<int, Expr>>> &trace_rules);
     void getTraceRules(ExprVector &all_k_vars, hyper_expr_map &k_vars, hyper_expr_map &k_rels,
-                        std::set<std::set<int>> &k_subsets, const HornClauseDB::RuleVector &rules,
-                        hyper_subset_expr_map &doomed_rels, HornClauseDB::RuleVector &trace_rules,
-                        HornClauseDB::expr_set_type &doomed_exprs_for_pre);
+                        std::set<std::set<int>> &k_subsets,
+                        std::map<const Function *, std::map<std::pair<int, int>, ExprVector[3]>>& trace_info,
+                        hyper_subset_expr_map &doomed_rels,
+                        std::map<const Function *, std::map<std::pair<int, int>, std::map<int, Expr>>> &trace_rules,
+                        std::map<const Function *, int> &max_pc,
+                        std::map<std::pair<int, int>, std::map<std::set<int>, Expr>> &final_trace_rules);
   public:
     static char ID;
     KPropertyVerifier (int hyper_k, bool interproc = false) : llvm::ModulePass (ID), hyper_k(hyper_k), m_interproc(interproc) {}
